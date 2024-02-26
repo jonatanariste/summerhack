b9b85fa690b897f5fef0a86b55193725e96225a1
Jonatan Ariste <jonatan.ariste@gmail.com>
2024-02-26 19:51:01 -0300

    Mi primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ComponenteNuevo from '../ComponenteNuevo'
+function App() {
+
+
+  console.log("HOLA DESDE REACT kekekeke")
+  return (
+    <>
+    <h1>HOla Mundo desde React!!!</h1>
+    <p>Te queremos Vite y React</p>
+    <ComponenteNuevo></ComponenteNuevo>
+    </>
+  )
+}
+
+export default App
